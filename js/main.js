f6a25b25910e31a2f2c2076f971392f0b32b9270
bambooka <sumkina.ol@yandex.ru>
2018-12-30 20:59:42 +0300

    Add items in infoblock

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 let block = document.createElement('div');
-let image = document.createElement('img');
-let title = document.createElement('h3');
+
 let goods = document.getElementById('goods_list');
 
-for (let i = 0; i <= 9; i++) {
-    image.setAttribute('src', 'https://via.placeholder.com/150');
-    title.innerHTML ='Example title';
+let content = {
+    infoblock: [
+        {
+            title: 'example',
+            img: 'https://via.placeholder.com/150'
+        },
+        {
+            title: 'example',
+            img: 'https://via.placeholder.com/150'
+        },
+        {
+            title: 'example',
+            img: 'https://via.placeholder.com/150'
+        },
+
+    ]
+};
+
+
+
+content.infoblock.forEach(function (item){
+    let image = document.createElement('img');
+    image.setAttribute('src', item.img);
     block.appendChild(image);
+    let title = document.createElement('h3');
+    title.innerHTML = item.title;
     block.appendChild(title);
     goods.appendChild(block);
-}
+    block.className = 'goods_item';
+});
